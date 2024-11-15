dff3bb5415992ebe68cbc4b71c59001b6e1ac565
naglicmoc <dvd.filmas@gmail.com>
2024-11-15 11:20:25 +0200

    geras intro

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+Pirma paleidžiam šitą komandos eilutę terminale
+pip install selenium
+'''
+
 import time
 
 from selenium import webdriver
@@ -11,11 +16,23 @@ driver.get("https://skelbiu.lt")
 sausainuku_mygtukas = driver.find_element(By.ID, 'onetrust-accept-btn-handler') #grazino web elementa
 sausainuku_mygtukas.click()
 
-driver.find_element(By.ID, 'searchKeyword').send_keys("lova")
-driver.find_element(By.NAME, 'submit_bn').click()
-# time.sleep(5)
-driver.find_element(By.XPATH, '//*[@id="popular_categories_by_keyword"]/div[1]/a[4]').click()
+#============================== paieska ======================
+# driver.find_element(By.ID, 'searchKeyword').send_keys("lova")
+# driver.find_element(By.NAME, 'submit_bn').click()
+# # time.sleep(5)
+# driver.find_element(By.XPATH, '//*[@id="popular_categories_by_keyword"]/div[1]/a[4]').click()
+#============================== paieska ======================
 
 
-time.sleep(120)
+#============================== kategorijų nuorodų nurinkimas į sarašą ======================
+main_container = driver.find_element(By.ID, 'main-categories-container')
+cage_blocks = main_container.find_elements(By.CLASS_NAME,'categBlock')
+hrefs = []
+for cb in cage_blocks:
+    for a in cb.find_elements(By.TAG_NAME, 'a'):
+        hrefs.append(a.get_attribute("href"))
+print(hrefs)
+#============================== kategorijų nuorodų nurinkimas į sarašą ======================
+
+time.sleep(600)
 driver.quit()
